d955707cc236ab43b0d07836f1c1cfa751badb08
JSand273 <jsandovalm14@miumg.edu.gt>
2024-03-01 23:51:58 -0600

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+        int a, b;
+        cout << "\n Ingrese numero a:";
+        cin >> a;
+        cout << "\n Ingrese numero b:";
+        cin >> b;
+        if (a == b)
+            cout << "\n Son iguales";
+        else {
+            if (a > b)
+                cout << "\n El mayor es: " << a;
+            else
+                cout << "\n El mayor es: " << b;
+        }
     return 0;
 }
